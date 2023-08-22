f42241c696eff95a7c21f16d0c68c4c6482a6ba4
HARISHIMSS <harishm@imss.co.in>
2023-08-22 15:19:25 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,146 @@
-# smh-rn-otp-textInput
+# smh-rn-otp-textinput
+
+smh-rn-otp-textinput
+
+## Installation
+```sh
+# Expo
+npx expo install smh-rn-otp-textinput
+
+#React Native
+npm install --save smh-rn-otp-textinput
+```
+
+## Helper Functions
+Clearing and Setting values to component
+
+```js
+// using traditional ref
+clearText = () => {
+    this.otpInput.clear();
+}
+
+setText = () => {
+    this.otpInput.setValue("1234");
+}
+
+render() {
+    return (
+        <View>
+            <OTPTextInput ref={e => (this.otpInput = e)} >
+            <Button title="clear" onClick={this.clearText}>
+        </View>
+    );
+}
+
+
+```
+
+
+```js
+// hooks
+import React, { useRef } from 'react';
+
+const ParentComponent = () => {
+    let otpInput = useRef(null);
+
+    const clearText = () => {
+        otpInput.current.clear();
+    }
+
+    const setText = () => {
+        otpInput.current.setValue("1234");
+    }
+
+    return (
+        <View>
+            <OTPTextView ref={e => (otpInput = e)} >
+            <Button title="clear" onClick={clearText}>
+        </View>
+    );
+}
+
+```
+
+## Example
+
+```js
+
+import React, { useRef, useState } from 'react'
+import { OtpTextInput, StyleSheet } from '@/imports';
+import { useTheme } from '@/hooks';
+import { heightPercent, widthPercent } from '@/helpers/functions/responsive';
+import { debugLog } from '@/config/logsConfig';
+import {OTPTextView, OTPTextViewType} from 'smh-rn-otp-textinput';
+
+const OtpInput = () => {
+
+  const theme = useTheme();
+  const { colors, fontSizes, iconSizes } = theme
+
+  const styles = StyleSheet.create({
+    textInputContainer: {
+      marginVertical: 15,
+      justifyContent:'center',
+      padding:widthPercent(0.3)
+    },
+    roundedTextInput: {
+      borderRadius: 10,
+      borderWidth: 1,
+      width:widthPercent(6),
+      height:heightPercent(6),
+      margin:widthPercent(0.2)
+    },
+  })
+
+  const [otpInput, setOtpInput] = useState<string>("");
+  const input = useRef<OTPTextViewType>(null);
+  const handleCellTextChange = async (text: string, i: number) => {
+    debugLog('handleCellTextChange')
+  };
+  return (
+    <OTPTextView
+      handleCellTextChange={handleCellTextChange}
+      handleTextChange={setOtpInput}
+      inputCount={6}
+      keyboardType="numeric"
+      containerStyle={styles.textInputContainer}
+      textInputStyle={styles.roundedTextInput}
+      tintColor={colors.text}
+    />
+  );
+
+}
+
+export default OtpInput
+
+```
+
+And we're done 🎉
+## Contributing
+
+Contribution are always welcome, no matter how large or small !
+
+We want this community to be friendly and respectful to each other.Please follow it in all your interactions with the project.
+
+Please feel free to drop me a mail [S MUNI HARISH](mailto:samamuniharish@gmail.com?subject=[GitHub])
+
+## Acknowledgements
+
+Thanks to the authors of these libraries for inspiration
+
+## Note
+
+Inspired by [react-native-otp-textinput](https://github.com/naveenvignesh5/react-native-otp-textinput#readme)
+
+## Sponsor & Support
+
+To keep this library maintained and up-to-date please consider [sponsoring it on GitHub](https://github.com/sponsors/smuniharish). Or if you are looking for a private support or help in customizing the experience, then reach out to me on Linkedin [@smuniharish](https://www.linkedin.com/in/smuniharish).
+
+## License
+
+[MIT](./LICENSE)
+
+---
+
+Made with ❤️
---
+++ src/index.tsx
@@ -0,0 +1,222 @@
+import React, { useState, useRef, useEffect } from 'react';
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  ViewStyle,
+  KeyboardType,
+  NativeSyntheticEvent,
+  TextInputKeyPressEventData,
+} from 'react-native';
+
+const DEFAULT_TINT_COLOR: string = '#3CB371';
+const DEFAULT_OFF_TINT_COLOR: string = '#DCDCDC';
+const DEFAULT_TEST_ID_PREFIX: string = 'otp_input_';
+const DEFAULT_KEYBOARD_TYPE: KeyboardType = 'numeric';
+
+interface OTPTextViewProps {
+  defaultValue?: string;
+  inputCount?: number;
+  containerStyle?: ViewStyle;
+  textInputStyle?: ViewStyle;
+  inputCellLength?: number;
+  tintColor?: string | string[];
+  offTintColor?: string | string[];
+  handleTextChange?(text: string): void;
+  handleCellTextChange?(text: string, cellIndex: number): void;
+  keyboardType?: KeyboardType;
+  testIDPrefix?: string;
+  autoFocus?: boolean;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  textInput: {
+    height: 50,
+    width: 50,
+    borderBottomWidth: 4,
+    margin: 5,
+    textAlign: 'center',
+    fontSize: 22,
+    fontWeight: '500',
+    color: '#000000',
+  },
+});
+
+const OTPTextView: React.FC<OTPTextViewProps> = ({
+  defaultValue = '',
+  inputCount = 4,
+  tintColor = DEFAULT_TINT_COLOR,
+  offTintColor = DEFAULT_OFF_TINT_COLOR,
+  inputCellLength = 1,
+  containerStyle = {},
+  textInputStyle = {},
+  handleTextChange = () => {},
+  handleCellTextChange = undefined,
+  keyboardType = DEFAULT_KEYBOARD_TYPE,
+  testIDPrefix = DEFAULT_TEST_ID_PREFIX,
+  autoFocus = false,
+}) => {
+  const getOTPTextChunks = (
+    inputCount: number,
+    inputCellLength: number,
+    text: string
+  ): string[] => {
+    const matches =
+      text.match(new RegExp(`.{1,${inputCellLength}}`, 'g')) || [];
+    return matches.slice(0, inputCount);
+  };
+  const [focusedInput, setFocusedInput] = useState<number>(0);
+  const [otpText, setOtpText] = useState<string[]>(
+    getOTPTextChunks(inputCount, inputCellLength, defaultValue)
+  );
+  const inputs = useRef<TextInput[]>([]);
+
+  useEffect(() => {
+    checkTintColorCount();
+  }, []);
+
+  const checkTintColorCount = () => {
+    if (Array.isArray(tintColor) && tintColor.length !== inputCount) {
+      throw new Error(
+        'If tint color is an array, its length should be equal to input count'
+      );
+    }
+
+    if (Array.isArray(offTintColor) && offTintColor.length !== inputCount) {
+      throw new Error(
+        'If off tint color is an array, its length should be equal to input count'
+      );
+    }
+  };
+
+  const basicValidation = (text: string) => {
+    const validText = /^[0-9a-zA-Z]+$/;
+    return text.match(validText);
+  };
+
+  const onTextChange = (text: string, i: number) => {
+    if (text && !basicValidation(text)) {
+      return;
+    }
+
+    const updatedOtpText = [...otpText];
+    updatedOtpText[i] = text;
+    setOtpText(updatedOtpText);
+
+    handleTextChange(updatedOtpText.join(''));
+
+    if (handleCellTextChange) {
+      handleCellTextChange(text, i);
+    }
+
+    if (text.length === inputCellLength && i !== inputCount - 1) {
+      inputs.current[i + 1]?.focus();
+    }
+  };
+
+  const onInputFocus = (i: number) => {
+    const prevIndex = i - 1;
+
+    if (prevIndex > -1 && !otpText[prevIndex] && !otpText.join('')) {
+      inputs.current[prevIndex]?.focus();
+      return;
+    }
+
+    setFocusedInput(i);
+  };
+
+  const onKeyPress = (
+    e: NativeSyntheticEvent<TextInputKeyPressEventData>,
+    i: number
+  ) => {
+    const val = otpText[i] || '';
+
+    if (e.nativeEvent.key !== 'Backspace' && val && i !== inputCount - 1) {
+      inputs.current[i + 1]?.focus();
+      return;
+    }
+
+    if (e.nativeEvent.key === 'Backspace' && i !== 0) {
+      const prevOtpText = [...otpText];
+      if (!val.length && prevOtpText[i - 1]?.length === inputCellLength) {
+        prevOtpText[i - 1] = prevOtpText[i - 1]!.slice(0, -1);
+        setOtpText(prevOtpText);
+        handleTextChange(prevOtpText.join(''));
+        inputs.current[i - 1]?.focus();
+      }
+    }
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const clear = () => {
+    setOtpText(Array(inputCount).fill(''));
+    handleTextChange('');
+    inputs.current[0]?.focus();
+  };
+ // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const setValue = (value: string, isPaste = false) => {
+    const updatedOtpText = getOTPTextChunks(inputCount, inputCellLength, value);
+    setOtpText(updatedOtpText);
+
+    const updatedFocusInput = isPaste ? inputCount - 1 : value.length - 1;
+    if (inputs.current[updatedFocusInput]) {
+      inputs.current[updatedFocusInput]?.focus();
+    }
+
+    handleTextChange(value);
+  };
+
+  const TextInputs = [];
+
+  for (let i = 0; i < inputCount; i += 1) {
+    const _tintColor = Array.isArray(tintColor) ? tintColor[i] : tintColor;
+    const _offTintColor = Array.isArray(offTintColor)
+      ? offTintColor[i]
+      : offTintColor;
+
+    const inputStyle = [
+      styles.textInput,
+      textInputStyle,
+      {
+        borderColor: _offTintColor,
+      },
+    ];
+
+    if (focusedInput === i) {
+      inputStyle.push({
+        borderColor: _tintColor,
+      });
+    }
+
+    TextInputs.push(
+      <TextInput
+        ref={(inputRef) => {
+          if (inputRef) {
+            inputs.current[i] = inputRef;
+          }
+        }}
+        key={i}
+        autoCorrect={false}
+        keyboardType={keyboardType}
+        autoFocus={autoFocus && i === 0}
+        value={otpText[i] || ''}
+        style={inputStyle}
+        maxLength={inputCellLength}
+        onFocus={() => onInputFocus(i)}
+        onChangeText={(text) => onTextChange(text, i)}
+        multiline={false}
+        onKeyPress={(e) => onKeyPress(e, i)}
+        selectionColor={_tintColor}
+        testID={`${testIDPrefix}${i}`}
+      />
+    );
+  }
+
+  return <View style={[styles.container, containerStyle]}>{TextInputs}</View>;
+};
+const OTPTextViewType = typeof OTPTextView;
+export { OTPTextView, OTPTextViewType };
